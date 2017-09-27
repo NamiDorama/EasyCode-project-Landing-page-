44379ae11284974fe141019b1fec1a89e4192b5d
Yuliia Voitiuk <yulliavoitiuk@gmail.com>
2017-09-27 10:40:28 +0300

    Landing page from PSD

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+
+    $(".carousel").owlCarousel({
+        singleItem: true,
+        navigation: true,
+        navigationText: false,
+        pagination: false
+    });
+
+    $('.btn_menu').click(function() {
+        $('.menu ul').slideToggle();
+    });
+
+    var mixer = mixitup('.mymix');
+
+
+});
